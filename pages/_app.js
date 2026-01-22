af4f7d8ffa615b33a8c28a6d5f775514df798060
TFPCOIN <tfpcoin@gmail.com>
2026-01-21 20:21:46 -0500

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-
-// OPTIONAL Privy support (will NOT crash if not installed or not configured)
-// If you want Privy later, you can install it and then uncomment the provider section below.
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
